95137a482f7a4db87f1586d8794f1978e41cffb9
hfungibm <hfung@hfungs-mbp.usca.ibm.com>
2018-01-08 17:37:11 -0800

    Add source

---
+++ src/policy/info/Policy.java
@@ -0,0 +1,13 @@
+package policy.info;
+
+public class Policy {
+	private int policyNumber;
+	
+	public int getPolicyNumber() {
+		return policyNumber;
+	}
+	
+	public void setPolicyNumber(int policyNumber) {
+		this.policyNumber = policyNumber;
+	}
+}
