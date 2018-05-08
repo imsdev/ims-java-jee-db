2487737999c32278b4f71e8bd574b319439cf9f8
Paul Hinze <phinze@us.ibm.com>
2018-05-08 12:57:48 -0700

    Changing ims-java-jee-db project to Maven project

--- src/application/InsuranceApplication.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/services/*")
-public class InsuranceApplication extends Application {
-
-}
